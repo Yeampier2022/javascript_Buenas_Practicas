89bd3b3771a19e7545a99f3e57658e9764de4b85
Yeampier2022 <yeampierjesusa@gmail.com>
2023-01-09 13:41:32 -0400

    Curso Finalizado Linkedin

---
+++ 3 - Variables, estructuras y objetos/3.12 - try_and_catch/app.js
@@ -0,0 +1,10 @@
+var animales = ["perro", "gato", "pez"];
+
+var totalAnimales = animales.length;
+
+try {
+  //Acciones a realizar por la excepción
+  for (var i = 0; i < totalAnimales; i++) {}
+} catch (e) {
+  //Manejo de errores
+}
